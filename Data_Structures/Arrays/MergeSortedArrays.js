d7fa5df7856236a54a773dd18719b6bce21a605d
Mazin Ashfaq <mzash00@gmail.com>
2021-11-29 07:58:11 -0800

    merged sorted arrays

--- Data_Structures/Arrays/MergeSortedArrays.js
+++ Data_Structures/Arrays/MergeSortedArrays.js
@@ -48,3 +48,46 @@ function MergeSortedArrays(arr1, arr2) {
 }
 
 console.log(MergeSortedArrays(arr1, arr2)); //(O(a*b*c))
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  m--; //start at end of array
+  n--; //start at end of array
+
+  for (let i = nums1.length - 1; i >= 0; i--) {
+    if (n < 0) {
+      break;
+    }
+    if (nums1[m] > nums2[n]) {
+      nums1[i] = nums1[m];
+      m--;
+    } else {
+      nums1[i] = nums2[n];
+      n--;
+    }
+  }
+};
+
+/*
+
+[1,2,3,0,0,0], m = 2, 
+    ^   
+[2,5,6], n = 2
+  ^     
+[1,2,3,0,0,6] i = 5
+          ^ 
+first compare m and n 
+if m is bigger, insert at end of first array
+  decrement m
+if n is bigger, insert at end of first array
+  decrement n
+if = insert m 
+
+loop until either m < 0 or n < 0
+*/
